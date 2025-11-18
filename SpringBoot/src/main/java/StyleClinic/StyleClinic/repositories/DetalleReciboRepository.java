c210f9e5dbf4f6ac83da7ca9044d6fdadfd3ee84
JuanD Bernal <juandimejor1@gmail.com>
2025-11-17 21:35:20 -0500

    SinSpring

--- SpringBoot/src/main/java/StyleClinic/StyleClinic/repositories/DetalleReciboRepository.java
+++
@@ -1,7 +0,0 @@
-package StyleClinic.StyleClinic.repositories;
-
-import StyleClinic.StyleClinic.models.DetalleRecibo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetalleReciboRepository extends JpaRepository<DetalleRecibo, Long> {
-}
--- SpringBoot/src/main/java/StyleClinic/StyleClinic/repositories/ReciboRepository.java
+++
@@ -1,7 +0,0 @@
-package StyleClinic.StyleClinic.repositories;
-
-import StyleClinic.StyleClinic.models.Recibo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReciboRepository extends JpaRepository<Recibo, Long> {
-}
